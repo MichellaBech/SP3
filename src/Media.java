6fa35e36f1ab37b4b29f2489e9c6cae7b821346a
mariaNoermoelle <cph-mn760@cphbusiness.dk>
2023-11-20 12:04:09 +0100

    MediaCategories

--- src/Media.java
+++ src/Media.java
@@ -7,28 +7,31 @@ import java.util.Scanner;
 
 public abstract class Media
 {
-    public ArrayList<String> scanMedia(String path)
+    public ArrayList<String> scanMediaCategories(String path)
     {
         ArrayList<String> data = new ArrayList();
         File file = new File(path);
+        String userInput = "";
         try
         {
             Scanner scan = new Scanner(file);
             while(scan.hasNextLine()) {
                 String s = scan.nextLine();
+                if(s.contains(userInput))
                 data.add(s);
             }
         } catch (FileNotFoundException e) {
             System.out.println("File not found");
-    }
+        }
         return data;
     }
 
-    public float Rating()
+
+/*    public float Rating()
     {
         float count = 0.0F;
         ArrayList<Media> rating = new ArrayList<>();
         return 0.0F;
     }
-
+*/
 }
