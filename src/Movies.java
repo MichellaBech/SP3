7ac8fedfc5c28d7b58b2cb6308b41b80376a9d5a
MB105 <124814937+MB105@users.noreply.github.com>
2023-11-23 10:40:35 +0100

    Update Movies.java

--- src/Movies.java
+++ src/Movies.java
@@ -1,4 +1,16 @@
 package src;
 
-public class Movies {
-}
+public class Movies extends Media {
+
+    private String title;
+
+    private String publication;
+
+
+    public Movies (String title,String publication){
+        super(title,publication);
+        this.title=title;
+        this.publication=publication;
+
+    }
+    }
